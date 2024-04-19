45de73aa196fdd08a9376c809aa4ea29198ddd44
e <6410762+e-mit@users.noreply.github.com>
2024-04-19 22:24:43 +0100

    Add dynamoDB to message system

--- function/app/routers/message.py
+++ function/app/routers/message.py
@@ -3,9 +3,26 @@ from datetime import datetime, timezone
 from typing import Annotated
 from typing_extensions import Self
 import uuid
+import logging
+import os
 
 from fastapi import APIRouter, status
 from pydantic import BaseModel, StringConstraints
+import boto3
+from boto3.dynamodb.conditions import Key
+
+DB_TABLE_NAME = os.environ['DB_TABLE_NAME']
+PK_VALUE = 1
+logger = logging.getLogger()
+
+if os.environ.get('TEST'):
+    logger.info('Local test mode: using database %s', DB_TABLE_NAME)
+    dynamo_table = boto3.resource('dynamodb',
+                                  endpoint_url='http://localhost:8000'
+                                  ).Table(DB_TABLE_NAME)
+else:
+    logger.info('Using database %s', DB_TABLE_NAME)
+    dynamo_table = boto3.resource('dynamodb').Table(DB_TABLE_NAME)
 
 
 class InputMessage(BaseModel):
@@ -14,30 +31,69 @@ class InputMessage(BaseModel):
 
 
 class StoredMessage(InputMessage):
-    id: int
+    id: str
     timestamp: datetime
 
     @classmethod
     def create(cls, message: InputMessage) -> Self:
-        msg = cls(name=message.name, text=message.text, id=uuid.uuid4().int,
+        msg = cls(name=message.name, text=message.text,
+                  id=str(uuid.uuid4().int),
                   timestamp=datetime.now(tz=timezone.utc))
         return msg
 
+    def post(self):
+        dynamo_table.put_item(
+            Item={"pk": PK_VALUE, "id": self.id,
+                  "timestamp": int(self.timestamp.timestamp()),
+                  "name": self.name, "text": self.text})
 
-messages: list[StoredMessage] = []
 
 router = APIRouter(tags=["Message"])
 
 
 @router.get("/")
 def read_messages() -> list[StoredMessage]:
-    """Get message(s)."""
-    return messages
+    """Get message(s), sorted by timestamp, most recent first."""
+    response = dynamo_table.query(
+        IndexName="gsi",
+        ScanIndexForward=False,
+        KeyConditionExpression=(
+            Key("pk").eq(PK_VALUE) & Key("timestamp").gt(0)))
+    return response['Items']
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def write_message(message: InputMessage):
     """Post a message."""
     msg = StoredMessage.create(message)
-    messages.append(msg)
+    msg.post()
     return msg
+
+
+@router.delete("/{id}")
+def delete_message(id: str):
+    """Delete a message."""
+    dynamo_table.delete_item(Key={"id": id})
+
+
+# As above but with pagination in groups of 2, and change query criteria
+# print()
+# results = []
+# scan_kwargs = {
+#     "IndexName": "gsi",
+#     "ScanIndexForward": False,
+#     "KeyConditionExpression": (
+#         Key("pk").eq(1) & Key("timestamp").lt(int(ts - 1))),
+#     "Limit": 2
+# }
+# done = False
+# start_key = None
+# while not done:
+#     if start_key:
+#         scan_kwargs["ExclusiveStartKey"] = start_key
+#     response = dynamo_table.query(**scan_kwargs)
+#     new_results = response.get("Items", [])
+#     print(new_results)
+#     results.extend(new_results)
+#     start_key = response.get("LastEvaluatedKey", None)
+#     done = start_key is None
