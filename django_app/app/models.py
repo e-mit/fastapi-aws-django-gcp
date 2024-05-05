3f77aea0c63dbbdf5a9721c4e41ef643da7c5778
e <6410762+e-mit@users.noreply.github.com>
2024-05-05 17:47:54 +0100

    Update model to convert timestamp

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -1,9 +1,11 @@
-from django.db.models import Model, CharField, DateTimeField
+from datetime import datetime, timezone
 
+from django.db.models import Model, CharField, DateTimeField, IntegerField
 
 MAX_NAME_LENGTH = 20
 MAX_SUBJECT_LENGTH = 40
 MAX_MESSAGE_LENGTH = 200
+MAX_ID_LENGTH = 39  # This is a 128-bit number
 
 
 class PostMessage(Model):
@@ -17,4 +19,15 @@ class DisplayMessage(Model):
     subject: CharField = CharField(max_length=MAX_SUBJECT_LENGTH)
     text: CharField = CharField(max_length=MAX_MESSAGE_LENGTH)
     timestamp: DateTimeField = DateTimeField("Date posted")
-    id: CharField = CharField(max_length=100, primary_key=True)
+    timestamp_ms: IntegerField = IntegerField()
+    id: CharField = CharField(max_length=MAX_ID_LENGTH, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+    @classmethod
+    def create(cls, **kwargs):
+        display_message = cls(**kwargs)
+        display_message.timestamp = datetime.fromtimestamp(
+            display_message.timestamp_ms/1000.0, tz=timezone.utc)
+        return display_message
