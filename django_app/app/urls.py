2f11b171b043d5ea6efd4199f6cb08c3ba06fdbe
e <6410762+e-mit@users.noreply.github.com>
2024-05-05 16:50:22 +0100

    Add simple view functions

--- django_app/app/urls.py
+++ django_app/app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+
+app_name = "app"
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("message/<int:message_id>/", views.message_detail,
+         name="message_detail")
 ]
--- django_app/app/views.py
+++ django_app/app/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.utils import timezone
+from django.shortcuts import render
+
+from .models import DisplayMessage
+from .forms import PostMessageForm
+
+
+test_message = DisplayMessage(name="bob", subject="hello",
+                              text="Body text", id="12345",
+                              timestamp=timezone.now().time())
 
 
 def index(request):
-    return HttpResponse("Hi.")
+    context = {"message_list": [test_message, test_message, test_message],
+               "PostMessageForm": PostMessageForm()}
+    if request.method == "POST":
+        message = PostMessageForm(request.POST)
+        if message.is_valid():
+            print(message.cleaned_data)
+        else:
+            context["errors"] = "Error: please check the form and retry."
+    return render(request, "app/index.html", context)
+
+
+def message_detail(request, message_id: int):
+    print(f"Get message {message_id}.")
+    return render(request, "app/message_detail.html",
+                  {"message": test_message})
