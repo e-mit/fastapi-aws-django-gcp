42499d7305a5fb681643b541ac115efd7a755c1f
e <6410762+e-mit@users.noreply.github.com>
2024-05-10 13:00:20 +0100

    Add 404 for message not found

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -2,6 +2,7 @@ import os
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.http import Http404
 from django.urls import reverse
 import requests
 
@@ -11,11 +12,12 @@ from .forms import PostMessageForm
 
 SITE_NAME = "FastAPI-AWS-Django-GCP"
 URL = "https://peil328b55.execute-api.eu-west-2.amazonaws.com"
-LIMIT = 20
+MESSAGE_DISPLAY_LIMIT = 20
 
 
 def index(request):
-    data = requests.get(os.path.join(URL, f"message?limit={LIMIT}"))
+    data = requests.get(
+        os.path.join(URL, f"message?limit={MESSAGE_DISPLAY_LIMIT}"))
     message_list = [DisplayMessage.create(**x) for x in data.json()]
     context = {"message_list": message_list,
                "PostMessageForm": PostMessageForm(),
@@ -35,10 +37,16 @@ def index(request):
 
 
 def message_detail(request, message_id: str):
-    try:
-        message = DisplayMessage.objects.get(id=message_id)
-    except DisplayMessage.DoesNotExist:
-        data = requests.get(os.path.join(URL, "message", message_id))
-        message = DisplayMessage.create(**data.json())
-    return render(request, "app/message_detail.html",
-                  {"message": message, "site_name": SITE_NAME})
+    if request.method == "GET":
+        try:
+            message = DisplayMessage.objects.get(id=message_id)
+        except DisplayMessage.DoesNotExist:
+            response = requests.get(os.path.join(URL, "message", message_id))
+            if response.status_code == int(requests.codes.not_found):
+                raise Http404("Message not found")
+            message = DisplayMessage.create(**response.json())
+        return render(request, "app/message_detail.html",
+                      {"message": message, "site_name": SITE_NAME})
+    elif request.method == "DELETE":
+        response = requests.delete(os.path.join(URL, "message", message_id))
+        DisplayMessage.objects.filter(id=message_id).delete()
