7c7ce897b2d1a9783e60e9cbe88e42d246d377a9
e <6410762+e-mit@users.noreply.github.com>
2024-05-12 11:28:51 +0100

    Remove the default colons from the form labels

--- django_app/app/forms.py
+++ django_app/app/forms.py
@@ -4,6 +4,13 @@ from .models import PostMessage
 
 
 class PostMessageForm(ModelForm):
+    """Define the form for posting a message."""
+
+    def __init__(self, *args, **kwargs):
+        """Remove the default colon from the form labels."""
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
+
     class Meta:
         model = PostMessage
         fields = ["name", "subject", "text"]
