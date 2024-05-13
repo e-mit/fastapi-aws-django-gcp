ee1f90f3d9300d64720f87d50d7ab380b1ed053d
e <6410762+e-mit@users.noreply.github.com>
2024-05-13 19:41:35 +0100

    Include linting and formatting checks in test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Run the FastAPI app locally, using a temporary
-# test dynamoDB instance, and run unit tests.
+# Run the FastAPI app locally, using a temporary test dynamoDB instance, and run unit tests.
+# Then also run static analysis and linting checks.
 
 rm -rf package
 
@@ -17,6 +17,15 @@ sleep 5
 
 ./create_test_table.sh
 
-python -m pytest tests -p no:cacheprovider
+python -m pytest --cov=function/app tests -p no:cacheprovider
 
 docker stop -t 0 dynamodb_test_local
+
+
+python -m bandit -r . --exclude=/tests/,/venv/,/cloud_tests/,message_tests.py
+python -m flake8 --exclude=tests/*,venv/*,cloud_tests/*,django_app/django_app/settings.py,django_app/app/migrations/*
+python -m mypy . --explicit-package-bases --exclude 'tests/' --exclude 'venv/' --exclude 'cloud_tests/'
+python -m pycodestyle function
+python -m pydocstyle function --ignore=D107,D203,D213
+python -m pylint function
+python -m pyright function
