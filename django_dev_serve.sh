88c88b365d1505cb140adec774e1042139b3d991
e <6410762+e-mit@users.noreply.github.com>
2024-06-26 16:20:29 +0100

    Remove DJANGO_SECRET_KEY and instead generate it in settings.py on each restart

--- django_dev_serve.sh
+++ django_dev_serve.sh
@@ -2,7 +2,6 @@
 
 # Run the development server, creating a fresh SQLite database
 
-export DJANGO_SECRET_KEY=$(openssl rand -base64 32)
 export DJANGO_DEBUG=1
 export DJANGO_ALLOWED_HOSTS="localhost 127.0.0.1"
 export API_URL="https://peil328b55.execute-api.eu-west-2.amazonaws.com"
